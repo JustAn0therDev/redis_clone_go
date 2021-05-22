a7a7089ef2c83e5d0b6f2b4ec394daa0f55450e5
Ruan Montelo <ruanmontelo@gmail.com>
2021-05-21 23:45:27 -0300

    Adding test directory with redis_test.go file

---
+++ test/redis_test.go
@@ -0,0 +1,81 @@
+package redis
+
+import (
+	"testing"
+)
+
+func TestGetValueShouldNotReturnError(t *testing.T) {
+	testString := "key"
+	testMap := map[string]string{"key": "value"}
+	result, err := GetValue(&testMap, &testString)
+	if err != nil {
+		t.Errorf("Expected return value %v. Got error: %v", result, err)
+	}
+}
+
+func TestGetValueShouldReturnError(t *testing.T) {
+	testString := ""
+	testMap := make(map[string]string)
+	result, err := GetValue(&testMap, &testString)
+	if err == nil {
+		t.Errorf("Expected error. Got: %v", result)
+	}
+}
+
+func TestDeleteValueShouldReturnError(t *testing.T) {
+	testString := ""
+	testMap := make(map[string]string)
+	result, err := DeleteValue(&testMap, &testString)
+	if err == nil {
+		t.Errorf("expected error, got result: %v", result)
+	}
+}
+func TestDeleteValueShouldNotReturnError(t *testing.T) {
+	testString := "key"
+	testMap := map[string]string{"key": "value"}
+	_, err := DeleteValue(&testMap, &testString)
+	if err != nil {
+		t.Errorf("expected error to be nil, got: %v", err)
+	}
+}
+
+func TestExistsShouldReturnOK(t *testing.T) {
+	testString := "key"
+	testValue := "val"
+	testMap := make(map[string]string)
+	result := SetValue(&testMap, &testString, &testValue)
+
+	if result != "ok" || len(testMap) != 1 {
+		panic("expected SetValue not to fail during Exists test")
+	}
+
+	result = Exists(&testMap, &testString)
+
+	if result == "nok" {
+		t.Error("expected Exists to return correctly, got nok")
+	}
+}
+
+func TestExistsShouldReturnNOK(t *testing.T) {
+	testString := ""
+	testMap := make(map[string]string)
+
+	result := Exists(&testMap, &testString)
+
+	if result != "nok" {
+		t.Error("expected Exists to return nok, got ok")
+	}
+}
+
+func TestFlushCommandShouldNotPanic(t *testing.T) {
+	testString := "key"
+	testValue := "val"
+	testMap := map[string]string{testString: testValue}
+	originalLength := len(testMap)
+
+	Flush(&testMap)
+
+	if len(testMap) == originalLength {
+		t.Error("expected the length of the map to be different after flush")
+	}
+}
